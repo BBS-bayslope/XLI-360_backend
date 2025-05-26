2c48c4b4a69c4552be789720c5d3f282e79c81cf
MayankR_Baylope <mayankrawat@bayslope.com>
2025-05-26 23:53:59 +0530

    ui_changes

---
+++ frontend/src/app/case-name-highlight.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CaseNameHighlightPipe } from './case-name-highlight.pipe';
+
+describe('CaseNameHighlightPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CaseNameHighlightPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
