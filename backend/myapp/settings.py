70bbe5ba0e420554e1f3ec999279747575284a58
MayankR_Baylope <mayankrawat@bayslope.com>
2025-07-21 17:05:45 +0530

    Add python-openai

--- backend/myapp/settings.py
+++ backend/myapp/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', '=)6fvl-@-)f$%bwd@fsda(yp2p#&$-!_*o@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['18.216.104.19', '18.220.232.127', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['18.216.104.19', '18.220.232.127', 'localhost', '127.0.0.1','xli-360-backend-1.onrender.com']
 
 # Google OAuth credentials (use environment variables in production)
 GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID', '511014243371-d0q42ehfg2l40rc722t0n6oep2hl08pd.apps.googleusercontent.com')
